46ca07524d040af034384b93d97158880285d905
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-02-01 12:14:51 +0000

    fix no return error

--- my-remix-app/app/root.tsx
+++ my-remix-app/app/root.tsx
@@ -22,15 +22,15 @@ export const links: LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
-export async function loader() {
+// export async function loader() {
   
-  // return Response.json({
-  //   ENV: {
-  //     STRIPE_PUBLIC_KEY:"MY_SECRET",
-  //     FAUNA_DB_URL: "FAUNA_DB_URL",
-  //   },
-  // });
-}
+//   return Response.json({
+//     ENV: {
+//       STRIPE_PUBLIC_KEY:"MY_SECRET",
+//       FAUNA_DB_URL: "FAUNA_DB_URL",
+//     },
+//   });
+// }
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
