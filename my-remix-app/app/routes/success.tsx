9df7a41d702e5ba60f96f2c556ed2074e8859a2b
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-02-01 06:01:34 +0000

    remove googleapis

--- my-remix-app/app/routes/success.tsx
+++ my-remix-app/app/routes/success.tsx
@@ -1,23 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import { google } from 'googleapis';
-
-const apiKey = process.env.REMIX_GMAIL_API_KEY
-const clientId = process.env.REMIX_GMAIL_CLIENT_ID
-const oauth2Client = new google.auth.OAuth2(
-    clientId,
-    apiKey,
-    'https://remixtutorial-flwf--5173--d20a0a75.local-credentialless.webcontainer.io/success'
-  );
-async function getTokens(code:string) {
-    const { tokens } = await oauth2Client.getToken(code);
-    console.log('Access Token:', tokens.access_token);
-    console.log('Refresh Token:', tokens.refresh_token);
-    oauth2Client.setCredentials(tokens);
-  }
-  
-  // You would typically get the code from the query parameter of the redirect URI
-  
-  
+import  { useEffect, useState } from 'react'
 function Success() {
     const [code, setCode] = useState<string | null>(null)
     useEffect(()=>{
@@ -25,11 +6,9 @@ function Success() {
         const codeText = params.get("code")
         setCode(codeText)
         console.log(codeText)
-        if(code)
-            getTokens(code)
     }, [])
   return (
-    <div >Success</div>
+    <div >Success {code} </div>
   )
 }
 
