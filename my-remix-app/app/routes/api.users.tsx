a50f1aa378783ad08d727fbbeac6e4957915dc68
Arunjadhav0101 <arunj5687@gmail.com>
2025-02-03 15:58:07 +0530

    init with shadecnui

--- my-remix-app/app/routes/api.users.tsx
+++
@@ -1,6 +0,0 @@
-import {  LoaderFunction } from '@remix-run/node';
-// This loader is the equivalent of an API endpoint
-export const loader: LoaderFunction = async () => {
-  return Response.json({ message: 'Hello, world!' });
-};
-
