7d88798486166bf877fea02fa324d3c1c1f4a084
sonu-dpu <23521208.acs@dypvp.edu.in>
2025-02-26 18:32:36 +0530

    chore: update ESLint configuration and add new Tailwind animations; include additional dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      "react-hooks/rules-of-hooks": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
