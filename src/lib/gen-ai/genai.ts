7f55153c7648bccff413dff4345464cc36077752
Sonu Shivcharan <sonushivcharan@outlook.com>
2025-06-24 13:17:16 +0530

    update gen ai model to gemini-2.0-flash

--- src/lib/gen-ai/genai.ts
+++ src/lib/gen-ai/genai.ts
@@ -18,7 +18,7 @@ const safetySettings = [
   },
 ];
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.0-flash",
   systemInstruction: instructions,
   safetySettings,
 });
