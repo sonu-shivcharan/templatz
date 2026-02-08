427dbe7126ecc49cb42fc892f70e7f6b9bc54d74
Sonu Shivcharan <sonushivcharan@outlook.com>
2026-02-08 15:26:36 +0530

    Update generative model from gemini-2.0 to gemini-2.5

--- src/lib/gen-ai/genai.ts
+++ src/lib/gen-ai/genai.ts
@@ -18,7 +18,7 @@ const safetySettings = [
   },
 ];
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  model: "gemini-2.5-flash",
   systemInstruction: instructions,
   safetySettings,
 });
